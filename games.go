f6ff231ba2b2373f4d9434f6c965f74161cb7ec1
BoppreH <boppreh@gmail.com>
2014-12-07 00:54:30 -0200

    Add support for non-Steam games
    Missing features: using the user-chosen label and finding their tags

--- games.go
+++ games.go
@@ -25,9 +25,10 @@ type Game struct {
 // inside Javascript, but this way is easier to extract.
 const profileGamePattern = `\{"appid":\s*(\d+),\s*"name":\s*"(.+?)"`
 
-// Returns all games from a given user, using both the public profile and local
-// files to gather the data. Returns a map of game by ID.
-func GetGames(user User) (games map[string]*Game, err error) {
+// Fetches the list of games from the public user profile. This is better than
+// looking locally because the profiles give the full game name, which can be
+// used for image searches later on.
+func addGamesFromProfile(user User, games map[string]*Game) (err error) {
 	profile, err := GetProfile(user)
 	if err != nil {
 		return
@@ -35,7 +36,6 @@ func GetGames(user User) (games map[string]*Game, err error) {
 
 	// Fetch game list from public profile.
 	pattern := regexp.MustCompile(profileGamePattern)
-	games = make(map[string]*Game, 0)
 	for _, groups := range pattern.FindAllStringSubmatch(profile, -1) {
 		gameId := groups[1]
 		gameName := groups[2]
@@ -44,16 +44,28 @@ func GetGames(user User) (games map[string]*Game, err error) {
 		games[gameId] = &Game{gameId, gameName, tags, imagePath, nil}
 	}
 
+	return
+}
+
+func addNonSteamGaems(user User, game map[string]*Game) {
+}
+
+// Loads the categories list. This finds the categories for the games loaded
+// from the profile and sometimes find new games, although without names.
+func addUnknownGames(user User, games map[string]*Game) {
 	// Fetch game categories from local file.
 	sharedConfFile := filepath.Join(user.Dir, "7", "remote", "sharedconfig.vdf")
 	if _, err := os.Stat(sharedConfFile); err != nil {
 		// No categories file found, skipping this part.
-		return games, nil
+		return
 	}
 	sharedConfBytes, err := ioutil.ReadFile(sharedConfFile)
+	if err != nil {
+		return
+	}
 
 	sharedConf := string(sharedConfBytes)
-	// VDF patterN: "steamid" { "tags { "0" "category" } }
+	// VDF pattern: "steamid" { "tags { "0" "category" } }
 	gamePattern := regexp.MustCompile(`"([0-9]+)"\s*{[^}]+?"tags"\s*{([^}]+?)}`)
 	tagsPattern := regexp.MustCompile(`"[0-9]+"\s*"(.+?)"`)
 	for _, gameGroups := range gamePattern.FindAllStringSubmatch(sharedConf, -1) {
@@ -74,6 +86,39 @@ func GetGames(user User) (games map[string]*Game, err error) {
 			}
 		}
 	}
+}
+
+// Adds non-Steam games that have been registered locally.
+func addNonSteamGames(user User, games map[string]*Game) {
+	// File that holds list of all non-Steam games.
+	file := filepath.Join(user.Dir, "760", "screenshots.vdf")
+	if _, err := os.Stat(file); err != nil {
+		// No custom games, skip this part.
+		return
+	}
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return
+	}
+
+	content := string(bytes)
+	// VDF pattern: "customid"		"name"
+	gamePattern := regexp.MustCompile(`"([0-9]+)"\s*"(.+?)"`)
+	for _, gameGroups := range gamePattern.FindAllStringSubmatch(content, -1) {
+		gameId := gameGroups[1]
+		name := gameGroups[2]
+		games[gameId] = &Game{gameId, name, []string{}, "", nil}
+	}
+}
+
+// Returns all games from a given user, using both the public profile and local
+// files to gather the data. Returns a map of game by ID.
+func GetGames(user User) (games map[string]*Game, err error) {
+	games = make(map[string]*Game, 0)
+
+	addGamesFromProfile(user, games)
+	addUnknownGames(user, games)
+	addNonSteamGames(user, games)
 
 	return
 }
