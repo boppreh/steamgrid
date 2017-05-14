974ed6dd38c9e0092181205ad3db640f173c2cc7
BoppreH <boppreh@gmail.com>
2017-05-13 22:57:51 -0300

    Mention sub-reddit as source of overlays

--- steamgrid.go
+++ steamgrid.go
@@ -32,7 +32,9 @@ func startApplication() {
 		errorAndExit(err)
 	}
 	if len(overlays) == 0 {
-		fmt.Println("No category overlays found. You can put overlay images in the folder 'overlays by category', where the filename is the game category.\n\nContinuing without overlays...")
+		fmt.Println("No category overlays found. You can put overlay images in the folder 'overlays by category', where the filename is the game category.\n\nYou can find many user-created overlays at https://wwww.reddit.com/r/steamgrid/wiki/overlays .\n\nContinuing without overlays...\n\n")
+	} else {
+		fmt.Println("Loaded %v overlays. \n\nYou can find many user-created overlays at https://wwww.reddit.com/r/steamgrid/wiki/overlays .\n\n", len(overlays))
 	}
 
 	fmt.Println("Looking for Steam directory...")
