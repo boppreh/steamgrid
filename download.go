e2e59b340f7aaa0b56e5203d679b03629410e582
strzlee <strzlee@gmx.net>
2019-11-11 12:18:01 +0100

    Fixed error check

--- download.go
+++ download.go
@@ -99,10 +99,10 @@ const SteamGridDBBaseURL = "https://www.steamgriddb.com/api/v2"
 func SteamGridDBGetRequest(url string, steamGridDBApiKey string) ([]byte, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
-	req.Header.Add("Authorization", "Bearer " + steamGridDBApiKey)
 	if err != nil {
 		return nil, err
 	}
+	req.Header.Add("Authorization", "Bearer " + steamGridDBApiKey)
 
 	response, err := client.Do(req)
 	if err != nil {
